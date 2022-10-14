9c1a7dfbb46cb2f06e9007b6f832167d1b14fbbf
GiraffeSummer <gidoselten@hotmail.com>
2022-10-14 13:47:57 +0200

    added service provider

---
+++ src/Providers/StopwatchProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace GiraffeSummer\Stopwatch\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class StopwatchProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
